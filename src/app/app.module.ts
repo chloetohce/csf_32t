76381ba4bf39e0edaafe5115244067a093411960
chloetohce <chloetohce@gmail.com>
2025-02-21 15:18:14 +0800

    done: order form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderComponent } from './components/order/order.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/order/order.component.ts
@@ -0,0 +1,57 @@
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-order',
+  standalone: false,
+  templateUrl: './order.component.html',
+  styleUrl: './order.component.css'
+})
+export class OrderComponent {
+  private fb = inject(FormBuilder)
+
+  protected orderForm!: FormGroup
+  protected availability!: FormArray
+  protected availabilityOptions = [
+    {id:0, value:"0900 - 1200"},
+    {id:1, value:"1200 - 1600"},
+    {id:2, value:"1600 - 2000"}
+  ]
+  protected items!: FormArray
+
+  ngOnInit(): void {
+    this.orderForm = this.createOrderForm()
+  }
+
+  protected isFieldValid(field: string): boolean {
+    return !!this.orderForm.get(field)?.valid
+  }
+
+  protected processOrderForm() {
+    console.info(this.orderForm.value)
+  }
+
+  protected handleAvailabilityOption(value: string, event: any) {
+    if (event.target.checked) {
+      this.availability.push(this.fb.group({value: this.fb.control<string>(value)}))
+    } else {
+      const index = this.availability.controls.findIndex(time => time.value == value)
+      this.availability.removeAt(index)
+    }
+  }
+
+  private createOrderForm() {
+    this.items = this.fb.array([])
+    this.availability = this.fb.array([])
+
+    return this.fb.group({
+      name: this.fb.control<string>('', [Validators.required, Validators.minLength(3)]),
+      address: this.fb.control<string>('', [Validators.required, Validators.minLength(3)]),
+      email: this.fb.control<string>('', [Validators.required, Validators.email]),
+      deliveryDate: this.fb.control<string>('', Validators.required),
+      availability: this.availability,
+      urgent: this.fb.control<boolean>(false),
+      items: this.items,
+    })
+  }
+}
